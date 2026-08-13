2a72b7220544b3f85d74ef8136365a448e47b705
Kishan S Patel <bishan.batel@protonmail.com>
2026-08-12 21:48:55 -0500

    Got basic cross compile working

--- flake.nix
+++ flake.nix
@@ -3,46 +3,59 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
-      self,
-      rust-overlay,
-      nixpkgs,
+    self,
+    rust-overlay,
+    flake-utils,
+    nixpkgs,
     }:
-
-    # TODO: Make this use the toolchain file for more compatibility
-    {
-      devShells.x86_64-linux.default =
+    flake-utils.lib.eachDefaultSystem (system: let 
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+      # TODO: Make this use the toolchain file for more compatibility
+      {
+      devShells.default =
         let
-          pkgsCross = nixpkgs.legacyPackages.x86_64-linux.pkgsCross.aarch64-multiplatform;
+          pkgsCross = pkgs.pkgsCross.aarch64-multiplatform;
+          platformDeps = (if pkgs.stdenv.isDarwin then with pkgsCross; [ 
+            libiconv 
+          ] else with pkgsCross; [ 
+              libdrm.dev 
+              libdecor.dev
+              mesa
+            ]);
           rust-bin = rust-overlay.lib.mkRustBin { } pkgsCross.buildPackages;
-        in
-        pkgsCross.callPackage (
-          {
-            mkShell,
-            pkg-config,
-            qemu,
-            openssl,
-            stdenv,
-          }:
+          in
+            pkgsCross.callPackage ( { mkShell, pkg-config, qemu, openssl, stdenv, }:
           mkShell {
             nativeBuildInputs = [
               (rust-bin.fromRustupToolchainFile ./toolchain.toml)
-              pkg-config
-            ];
+                pkg-config
+              ] ++ platformDeps;
 
-            depsBuildBuild = [ qemu ];
-            buildInputs = [ openssl ];
+              depsBuildBuild = [ qemu ];
 
-            env = {
-              CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER = "${stdenv.cc.targetPrefix}cc";
-              CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_RUNNER = "qemu-aarch64";
-              CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
-              CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUNNER = "qemu-aarch64";
-            };
-          }
-        ) { };
-    };
+              buildInputs = [ 
+                pkg-config
+                openssl 
+                pkgsCross.stdenv.cc
+              ] ++ 
+              [
+                pkgs.cmake
+              ];
+
+              env = {
+                LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [ ]);
+                DYLD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [ ]);
+                
+                CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${pkgsCross.stdenv.cc.targetPrefix}cc";
+                CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUNNER = "qemu-aarch64";
+              };
+            }
+          ) { };
+    });
 }
